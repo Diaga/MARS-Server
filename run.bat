4f9f805d52e1d55699beec6e4c29617298fcd7b9
Maaz Bin Tahir <diagaaos@gmail.com>
2019-12-20 23:20:54 +0500

    Added setup and run bat files

---
+++ run.bat
@@ -0,0 +1,4 @@
+CALL cd venv/Scripts
+CALL activate
+CALL cd ../..
+CALL python app/manage.py runserver
---
+++ setup.bat
@@ -0,0 +1,6 @@
+CALL pip install virtualenv
+CALL virtualenv venv
+CALL cd venv/Scripts
+CALL activate
+CALL cd ../..
+CALL pip install -r requirements.txt
